14b7a0e99b3657214a645dce75f3e6275ce3def3
Navaneethan <navaneethan@trackerwave.com>
2024-12-26 22:10:32 +0530

    web

---
+++ Billing-client/js/scripts.js
@@ -0,0 +1,20 @@
+function showForm(){
+    let popUp = document.getElementById('pop-up')
+
+    popUp.style.display="block"
+}
+
+function addPurchase(){
+
+    let popUp = document.getElementById('pop-up')
+
+    popUp.style.display="none"
+
+}
+
+function empty(){
+    document.getElementById('content').textContent="Nothing to Show......"
+    
+}
+
+
